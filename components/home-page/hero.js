0a27f45f3b533ee9f1cde03797b37283f746b4b7
Abdullah-encrypts <Dev.AbdullahAwan@gmail.com>
2023-08-06 00:15:37 +0500

    the site is complete now without bugs

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -7,16 +7,15 @@ function Hero() {
     <section className={classes.hero}>
       <div className={classes.image}>
         <Image
-          src='/images/site/max.png'
-          alt='An image showing Max'
+          src='/images/site/Me.jpg'
+          alt='Image showing the master developer Abdullah!'
           width={300}
           height={300}
         />
       </div>
-      <h1>Hi, I'm Max</h1>
+      <h1>Hi, I'm Abdullah</h1>
       <p>
-        I blog about web development - especially frontend frameworks like
-        Angular or React.
+        I am a front end developer who also aspires AI and cloud computing!
       </p>
     </section>
   );
